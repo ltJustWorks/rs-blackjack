b0caeb122375b252f9ad5295af81ccd294dd1d3d
Manuel <43526682+ltJustWorks@users.noreply.github.com>
2023-05-27 13:22:57 -0400

    add cards, card values

--- blackjack/src/main.rs
+++ blackjack/src/main.rs
@@ -1,3 +1,43 @@
+enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Jack,
+    Queen,
+    King,
+}
+
+enum Suit {
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs,
+}
+
+struct Card {
+    rank: Rank,
+    suit: Suit,
+    value: i32,
+}
+
+fn card_value(card: &Card, cards_total: i32) -> i32 {
+    match card.rank {
+        Rank::Ace => {
+            if cards_total + 11 <= 21 {11} else {1}
+        }
+        Rank::Two..=Rank::Nine => rank as i32,
+        Rank::Ten..=Rank::King => 10,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck: Vec<Card> = Vec::new();
+
+    println!("Blackjack");
 }
